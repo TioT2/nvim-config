54331db0d534063e8d058ca3359ad0341116ae4c
TioT2 <piopi4112@gmail.com>
2024-09-20 15:45:48 +0300

    Splitted editor.lua file

--- init.lua
+++ init.lua
@@ -1,9 +1,5 @@
 -- configuration root file
 
-vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.smarttab = true
-vim.opt.smartindent = true
+require("editor")
 
 -- file init.lua
---
+++ lua/editor.lua
@@ -0,0 +1,17 @@
+-- Editor configuration main file
+
+local o = vim.opt
+
+o.number = true
+o.relativenumber = true
+o.clipboard = "unnamedplus"
+
+o.termguicolors = true -- introduce colors to terminal gui
+o.expandtab = true
+o.tabstop = 4
+o.shiftwidth = 4
+o.smarttab = true
+o.smartindent = true
+
+-- file editor.lua
+
