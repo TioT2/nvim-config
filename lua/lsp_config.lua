eb53f09375f076df0352855f8e9367ca3bfcf3c2
TioT2 <piopi4112@gmail.com>
2024-10-27 02:12:24 +0300

    Lazy setup fixed.

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -10,6 +10,7 @@ require("mason-lspconfig").setup({
     ensure_installed = {
         "lua_ls",
         "rust_analyzer",
+        "clangd",
     },
 })
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,10 +6,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system({
         "git",
         "clone",
-        "--filter=blob:none",
+--         "--filter=blob:none",
         "https://github.com/folke/lazy.nvim.git",
         "--branch=stable",
-        "--depth=1",
+--        "--depth=1",
         lazypath,
     })
 end
@@ -17,29 +17,28 @@ vim.opt.rtp:prepend(lazypath)
 
 -- initialize plugins
 local plugins = {
-    {
-        "catppuccin/nvim",
-    },
-    {
-        "williamboman/mason.nvim",
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-    },
-    {
-        "neovim/nvim-lspconfig",
-    },
+    -- plugins
+    "catppuccin/nvim",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+
+    -- cmp utils
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/nvim-cmp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
+
+    -- tree
     {
         "nvim-tree/nvim-tree.lua",
         config = function()
             require("nvim-tree").setup({})
         end,
     },
+
+    -- syntax highlighting
     {
         "nvim-treesitter/nvim-treesitter",
         config = function()
